3b216ddc87aae46dff46ca2aac1a5bd9c5706d65
NathanB <nate19981@googlemail.com>
2022-03-27 21:05:15 +0100

    Clear canvas button added, logic not complete.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const gridContainer = document.getElementById("gridContainer");
+const gridContainer = document.getElementById('gridContainer');
+const clearBtn = document.getElementById('clearBtn');
 
 function makeRows(rows, cols) {
     gridContainer.style.setProperty('--grid-rows', rows);
     gridContainer.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
-      let cell = document.createElement("div");
-      gridContainer.appendChild(cell).className = "grid-item";
+      let cell = document.createElement('div');
+      gridContainer.appendChild(cell).className = 'grid-item';
 
       gridContainer.addEventListener('mouseover', changeColor);
     };
   };
 
 function changeColor(e) {
-    e.target.style.backgroundColor = '#000000';
-}
+    e.target.style.backgroundColor = '#000000';}
 
-  makeRows(16, 16);
+makeRows(16, 16);
