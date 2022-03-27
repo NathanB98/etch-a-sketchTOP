955ce751cfa7d0e55f9db2a7fcd2afcccd4cc75e
NathanB <nate19981@googlemail.com>
2022-03-27 19:34:23 +0100

    16x16 Grid showing

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-createGrid();
+const container = document.getElementById("gridContainer");
 
-function createGrid() {
-    for(let i = 0; i < 16; i++){
-        let gridSquare = document.createElement('div');
-        gridSquare.classList.add('squareDiv');
-        document.getElementById('gridContainer').appendChild(gridSquare);
-    }
-}
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < (rows * cols); i++) {
+      let cell = document.createElement("div");
+      cell.innerText = (i + 1);
+      container.appendChild(cell).className = "grid-item";
+    };
+  };
+  
+  makeRows(16, 16);
